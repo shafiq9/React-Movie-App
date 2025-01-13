c2b3e731bd82977f3afd7c8a47eda11e8fe1a21d
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2025-01-13 23:20:16 +0500

    structured MovieCard Component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,29 @@
+
+
+function MovieCard({movie}){
+
+    function onFavoriteClick(){
+        alert("clicked")
+    }
+
+    return
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.title} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>
+                        H
+                    </button>
+
+                </div>
+            </div>
+
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+
+        </div>
+}
+
+export default MovieCard
